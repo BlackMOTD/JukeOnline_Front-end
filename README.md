6b211226f236a82299e0898dde79b0ee1ce5680b
BlackMOTD <105824316+BlackMOTD@users.noreply.github.com>
2024-05-03 13:39:57 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,40 @@
+Site conçu en html, javascript et css
+Prévu comme lecteur audio, il suffit de mettre : 
+- le son mp3 dans le dossier upload/music
+- la cover dans le dossier upload/cover
+
+Sans oublier de modifier le fichier /assets/json/data.json , sous forme : 
+
+    {
+        "id" : X, //ID unique
+        "artiste" : "NomArtiste",  
+        "cover" : "cover.jpeg", //Mettre la bonne extension
+        "sound" : "musique.mp3", //En MP3 de préférence
+        "title" : "titreMusique", 
+        "category" : "typeMusique"
+
+    },
+    
+Le site s'affiche ainsi sur ordinateur : 
+- au format "carte" :
+<img width="800" alt="ordi1" src="https://github.com/BlackMOTD/JukeBox/assets/105824316/4b2c9427-8d69-4077-a406-8396908c366a">
+
+- au format "liste" : 
+<img width="800" alt="ordi2" src="https://github.com/BlackMOTD/JukeBox/assets/105824316/d6f4c051-58bd-46c4-bcb7-411180cbbf36">
+
+
+Et sur mobile : 
+- au format "carte" :
+<img width="300" alt="tel1" src="https://github.com/BlackMOTD/JukeBox/assets/105824316/3526dacc-37ec-4567-9b03-cb4ed6cc6e31">
+
+
+- au format "liste" : 
+<img width="300" alt="tel1" src="https://github.com/BlackMOTD/JukeBox/assets/105824316/645c793d-9615-4e5f-aa31-a0b336f25fb9">
+
+
+En appuyant sur une étiquette de musique, la musique se lancera automatiquement.
+Une fois la musique lancé, le titre aura une petite animation qui pourra nous montrer que la musique est en lecture.
+Il est possible de mettre en pause et reprendre la musique en appuyant sur le bouton "Play" et "Pause", quand le pause est activé le titre restera fixe.
+Un bouton aléatoire va permettre la lecture d'une musique aléatoirement dans la liste.
+Le dernier bouton permet de changer de mode de vues carte/liste.
+
---
+++ assets/js/app.js
@@ -0,0 +1,107 @@
+// app.js
+
+const audioPlayer = document.getElementById('audioPlayer');
+const lecteur = document.querySelector('#audioPlayer');
+const playlist = document.getElementById("playlist");
+const playPauseButton = document.getElementById('playPauseButton');
+const titreEnCoursText = document.getElementById("titre-en-cours-text");
+const vueButton = document.getElementById('vue-button');
+const themeButton = document.getElementById('theme-button');
+const container = document.getElementById('container');
+const config = {
+    urlCover: "./upload/cover/",
+    urlSound: "./upload/music/",
+    urlIcone: "./assets/ico/"
+};
+
+let allLi;
+let vuePlaylist = 'default'; // 'default' ou 'liste'
+
+// Affichage des musiques 
+const getData = async () => {
+    const req = await fetch("https://api-music-dblocal.onrender.com/api/v1/music");
+    console.log(req);
+    const dbMusic = await req.json();
+    data = dbMusic.result;
+    console.log("result ", data);
+    data.forEach((music) => {
+        if (vuePlaylist === 'liste') {
+playlist.innerHTML += `
+    <li class="playlist-item liste" id="${music.id}">
+        <img class="cover" src="${config.urlCover}${music.cover}" alt="${music.title}">
+        <div class="music-details">
+            <h2 class="playlist-music-details-text">${music.title}  -  </h2>
+            <h3 class="playlist-music-details-text">${music.artiste}</h3>
+        </div>
+        <div class="play-icon"></div> <!-- Ajout de l'élément pour l'icône play -->
+    </li>
+`;
+        } else { // Affichage par défaut
+            playlist.innerHTML += `
+                <li class="playlist-item default" id="${music.id}">
+                    <div class="music-details">
+                        <h2 class="playlist-music-title">${music.title}</h2>
+                        <h3 class="playlist-music-artist">${music.artiste}</h3>
+                    </div>
+                    <img class="cover" src="${config.urlCover}${music.cover}" alt="${music.title}">
+                </li>
+            `;
+        }
+    });
+    allLi = document.querySelectorAll(".playlist-item");
+
+    allLi.forEach((li) => {
+        li.addEventListener("click", function(elem) {
+            const id = parseInt(li.id);
+            const searchById = data.find((element) => element.id === id);
+            if (searchById) {
+                console.log(searchById);
+                titreEnCoursText.textContent = `${searchById.artiste} - ${searchById.title}`;
+               // alert(`Veux-tu écouter le titre : ${searchById.title}`);
+                lecteur.src = `${config.urlSound}${searchById.sound}`;
+                lecteur.play();
+                playPauseButton.style.backgroundImage = 'url("' + config.urlIcone + 'pause.png")';
+                titreEnCoursText.style.animation = 'colorAnimation 5s infinite'; // Démarrer l'animation
+            } else {
+                console.log("Aucune musique trouvée avec cet ID.");
+            }
+        });
+    });
+}
+
+// ------------------------------ HEADER ---------------------------------
+
+let animationPlayState = 'paused'; // État de l'animation
+
+// PLAY - PAUSE
+playPauseButton.addEventListener('click', function() {
+    if (audioPlayer.paused) {
+        audioPlayer.play();
+        playPauseButton.style.backgroundImage = 'url("' + config.urlIcone + 'pause.png")';
+        titreEnCoursText.style.animationPlayState = animationPlayState; // Reprendre l'animation
+    } else {
+        audioPlayer.pause();
+        playPauseButton.style.backgroundImage = 'url("' + config.urlIcone + 'play.png")';
+        animationPlayState = getComputedStyle(titreEnCoursText).animationPlayState; // Obtenir l'état de l'animation
+        titreEnCoursText.style.animationPlayState = 'paused'; // Mettre en pause l'animation
+    }
+});
+
+//RANDOM
+const playRandomMusic = () => {
+    const randomIndex = Math.floor(Math.random() * allLi.length);
+    const randomLi = allLi[randomIndex];
+    randomLi.click();
+};
+const randomButton = document.querySelector('#random-button');
+randomButton.addEventListener('click', playRandomMusic);
+
+// Ajoutez un gestionnaire d'événements au bouton "vue-button" pour basculer entre les vues
+vueButton.addEventListener('click', () => {
+    vuePlaylist = vuePlaylist === 'default' ? 'liste' : 'default';
+    playlist.innerHTML = ''; // Efface le contenu actuel de la playlist
+    getData(); // Regénère la playlist avec la nouvelle vue
+});
+
+
+getData();
